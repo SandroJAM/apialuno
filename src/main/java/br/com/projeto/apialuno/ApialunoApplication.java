c5aa1ef802ffae5e57ce9c4a8bf0cba8178b10fb
Sandro Muniz <sandrojam@hotmail.com>
2024-08-05 21:07:45 -0300

    Initial commit

---
+++ src/main/java/br/com/projeto/apialuno/ApialunoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.apialuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApialunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApialunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/apialuno/ApialunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.apialuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApialunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
