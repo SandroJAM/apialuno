395c7c0197c79b17b2b6367a88b50bc203dcf52b
Sandro Muniz <sandrojam@hotmail.com>
2024-08-05 22:10:27 -0300

    @Autowired e @Service
    Annotation @Autowired, responsável pela injeção de dependência.
    Annotation @Service, responsável em termos uma camada de serviços.

--- src/main/java/br/com/projeto/apialuno/controle/Controle.java
+++ src/main/java/br/com/projeto/apialuno/controle/Controle.java
@@ -1,5 +1,14 @@
 package br.com.projeto.apialuno.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.apialuno.repositorio.Repositorio;
+
+@RestController
 public class Controle {
+
+    @Autowired
+    private Repositorio acao;
     
 }
---
+++ src/main/java/br/com/projeto/apialuno/servico/Servico.java
@@ -0,0 +1,10 @@
+package br.com.projeto.apialuno.servico;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Servico {
+
+    
+    
+}
