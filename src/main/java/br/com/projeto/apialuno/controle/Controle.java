13714bfcb63a6f638d909bdad20eda4266e34c62
Sandro Muniz <sandrojam@hotmail.com>
2024-08-05 23:00:06 -0300

    Implementando serviços (Inserir)
    Método de cadastro na classe de serviços, criando uma validação nos dados recebidos através das requisições posts.

--- src/main/java/br/com/projeto/apialuno/controle/Controle.java
+++ src/main/java/br/com/projeto/apialuno/controle/Controle.java
@@ -1,14 +1,27 @@
 package br.com.projeto.apialuno.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.projeto.apialuno.repositorio.Repositorio;
+import br.com.projeto.apialuno.modelo.Aluno;
+// import br.com.projeto.apialuno.repositorio.Repositorio;
+import br.com.projeto.apialuno.servico.Servico;
 
 @RestController
 public class Controle {
 
+    // @Autowired
+    // private Repositorio acao;
+
     @Autowired
-    private Repositorio acao;
+    private Servico servico;
+
+    @PostMapping("/apiAluno")
+    public ResponseEntity<?> inserir(@RequestBody Aluno objAluno){
+        return servico.inserir(objAluno);
+    }
     
 }
--- src/main/java/br/com/projeto/apialuno/servico/Servico.java
+++ src/main/java/br/com/projeto/apialuno/servico/Servico.java
@@ -1,10 +1,36 @@
 package br.com.projeto.apialuno.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import br.com.projeto.apialuno.modelo.Aluno;
+import br.com.projeto.apialuno.modelo.Mensagem;
+import br.com.projeto.apialuno.repositorio.Repositorio;
 
 @Service
 public class Servico {
 
-    
+    @Autowired
+    private Mensagem mensagem;
+
+    @Autowired
+    private Repositorio acao;
+
+    public ResponseEntity<?> inserir(Aluno objAluno){
+        if(objAluno.getNome().equals("")){
+            mensagem.setMensagem("O nome do Aluno precisa ser preenchido!");
+            return new ResponseEntity<>(mensagem, HttpStatus.BAD_REQUEST);
+        }else if(objAluno.getNota1() < 0){
+            mensagem.setMensagem("Informe uma Nota válida!");
+            return new ResponseEntity<>(mensagem, HttpStatus.BAD_REQUEST);
+        }else if(objAluno.getNota2() < 0){
+            mensagem.setMensagem("Informe uma Nota válida!");
+            return new ResponseEntity<>(mensagem, HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<>(acao.save(objAluno), HttpStatus.OK);
+        }
+    }
     
 }
